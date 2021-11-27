c1cdfae336ef69703671d6830452c67df886ccf8
SamarpanCoder2002 <samarpan2dasgupta@gmail.com>
2021-11-27 11:10:58 +0530

    Infinite Looading Working Fine

--- src/components/image-container.js
+++ src/components/image-container.js
@@ -7,13 +7,25 @@ import { useDispatch, useSelector } from "react-redux";
 
 const ImageContainer = () => {
   const [images, setimages] = useState([]);
+  const [prevQuery, setprevQuery] = useState("");
+  const [bottomLoading, setbottomLoading] = useState(false);
   const [page, setpage] = useState(1);
 
   const { query, isLoading } = useSelector((state) => {
+    const { query } = state;
+    if (query && query !== prevQuery) {
+      setimages([]);
+      setpage(1);
+      setprevQuery(query);
+    }
     return state;
   });
 
   const dispatch = useDispatch();
+
+  const startLoading = () => {
+    dispatch({ type: "START_LOADING" });
+  };
 
   const stopLoading = () => {
     dispatch({ type: "STOP_LOADING" });
@@ -22,7 +34,7 @@ const ImageContainer = () => {
   useEffect(() => {
     if (query && query !== "") {
       fetch(
-        `https://api.pexels.com/v1/search?query=${query}&per_page=10&page=${page}`,
+        `https://api.pexels.com/v1/search?query=${query}&per_page=15&page=${page}`,
         {
           method: "GET",
           headers: {
@@ -31,29 +43,48 @@ const ImageContainer = () => {
           },
         }
       )
-        .then((response) => {
-          response
-            .json()
-            .then((data) => {
-              const imageCollection = [];
-              data.photos.map((imageBox) =>
-                imageCollection.push(imageBox.src.large)
-              );
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          const imageCollection = [];
+          data.photos.map((imageBox) =>
+            imageCollection.push(imageBox.src.large)
+          );
 
-              //// Conditional Insertion based on the page number
-              if (page === 1) {
-                setimages(imageCollection);
-              } else {
-                setimages([...images, ...imageCollection]);
-              }
-            })
-            .then(() => {
-              stopLoading();
-            });
+          //// Conditional Insertion based on the page number
+          console.log("Page: ", page);
+          setimages([...images, ...imageCollection]);
+          stopLoading();
+          setbottomLoading(false);
         })
-        .catch(setimages([]));
+        .then(() => {});
+
+      // .catch(setimages([]));
     }
   }, [query, page]);
+
+  const scrollToEnd = () => {
+    // startLoading();
+    console.log("at bottom");
+    setbottomLoading(true);
+    setpage(page + 1);
+  };
+
+  window.onscroll = () => {
+    console.log("Scrolling");
+    console.warn(window.innerHeight);
+    console.warn(Math.round(window.scrollY));
+    console.error(document.documentElement.offsetHeight);
+    if (
+      window.innerHeight + Math.round(window.scrollY) >=
+        document.documentElement.offsetHeight ||
+      window.innerHeight + Math.round(window.scrollY) ===
+        document.documentElement.offsetHeight - 1
+    ) {
+      console.log("Detected");
+      scrollToEnd();
+    }
+  };
 
   const breakpointColumnsObj = {
     default: 3,
@@ -81,6 +112,20 @@ const ImageContainer = () => {
             </div>
           )}
         </Masonry>
+        {/* {(
+          <div style={{ textAlign: "center", marginTop: "20px" }}>
+            <div
+              style={{
+                border: "1px solid #fff",
+                padding: "10px",
+                borderRadius: "5px",
+                color: "#fff"
+              }}
+            >
+              Loading...
+            </div>
+          </div>
+        )} */}
       </Container>
     </Base>
   );
